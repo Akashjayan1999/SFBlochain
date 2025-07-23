f254cbd8bac9a46f2c3ca9ed19a0913160028217
Akashjayan1999 <akashjayan999@gmail.com>
2025-07-23 16:07:19 +0000

    add ws for peer to peer connection and live blockchain

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Block = require('../blockchain/block');
 const Blockchain = require('../blockchain');
 const bodyParser = require('body-parser');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
@@ -9,6 +10,7 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 app.use(bodyParser.json()); 
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
@@ -27,3 +29,4 @@ app.post('/mine', (req, res) => {
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`);
 }); 
+p2pServer.listen();

7ada070576d4392b4621012aaf1f8402e651b620
Akashjayan1999 <akashjayan999@gmail.com>
2025-07-30 16:04:38 +0000

    add wallet

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const DIFFICULTY = 4;
 const MINE_RATE = 3000; // milliseconds
-
+const INITIAL_BALANCE = 500;
 module.exports = {
     DIFFICULTY,
-    MINE_RATE
+    MINE_RATE,
+    INITIAL_BALANCE
 };
